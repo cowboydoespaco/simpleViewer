6de74fddcbd48d4faee381a97cb1c94ed78bf140
Henrique Magalhães <henrique.albertino@gmail.com>
2013-03-02 19:54:24 -0300

    Mashed everything on main. Added thumb creation.

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect
 from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug import secure_filename
-import os
+import os, Image
 
 app = Flask(__name__)
 app.config.from_pyfile('main.cfg')
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
 db = SQLAlchemy(app)
+
+class Imagem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(50))
+    nomeArquivo = db.Column(db.String(50))
+    
+    def __init__(self, titulo, nomeArquivo):
+        self.titulo = titulo
+        self.nomeArquivo = nomeArquivo
+
+    def __repr__(self):
+        return '<Imagem: %r>' % self.titulo
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -22,16 +35,26 @@ def index():
 def requisicao():
     file = request.files['file']
     if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        alert_type = 'alert-success'
-        flash(u'Sua foto foi enviada com sucesso.')
+        salvarImagem(file, request.form['titulo'], file.filename)
+        flash(u'Sua foto foi enviada com sucesso.', 'alert-success')
+        return redirect('/')
     else:
-        alert_type = 'alert-error'
-        flash(u'Tipo de arquivo não permitido.')
+        flash(u'Tipo de arquivo não permitido.', 'alert-error')
+        return redirect('/')
 
-    template = 'index.html'
-    return render_template('base.html', **locals())
+def salvarImagem(file, titulo, nomeArquivo):
+    filename = secure_filename(file.filename)
+    save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(save_path)
+    generateThumbnail(save_path, "thumb_"+filename)
+    imagem = Imagem(secure_filename(request.form['titulo']), filename)
+    db.session.add(imagem)
+    db.session.commit()
+
+def generateThumbnail(save_path, thumb_name):
+    im = Image.open(save_path)
+    im.thumbnail((100,100))
+    im.save(os.path.join(app.config['UPLOAD_FOLDER'], thumb_name))
 
 if __name__ == "__main__":
     app.run()
